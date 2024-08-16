23c97905a4d272288d76fec76b5b1ffd3b92e7d6
jakscha <mails-straeucher0l@icloud.com>
2024-08-17 00:08:40 +0200

    little change for pr

--- .github/workflows/result_validation.py
+++ .github/workflows/result_validation.py
@@ -3,32 +3,75 @@ import os
 
 
 class ResultValidation:
+
+    def __init__(self):
+        self.count = 0
+
     def load_json_from_file(self, file_name: str) -> dict:
         with open(f'.github/workflows/results/{file_name}', "r") as f:
             return json.load(f)
 
-    def set_env_variable(self):
-        with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
-            env_file.write('SCAN_VALID=true\n')
     def validate_zap(self):
-        pass
+        valid = True
+        data = self.load_json_from_file('zap_scan/report_json.json')
+        findings = data['Results']
+        print(type(findings))
+        for result in data['site']:
+            for finding in result['alerts']:
+                if 'High' in finding['riskdesc'] or 'Critical' in finding['Severity']:
+                    self.count += 1
+                    valid = False
+        return valid
 
     def validate_nuclei(self):
-        pass
+        return True
 
     def validate_trivy(self):
+        valid = True
         data = self.load_json_from_file('trivy.json')
         findings = data['Results']
         print(type(findings))
         for result in data['Results']:
             for finding in result['Vulnerabilities']:
                 if finding['Severity'] == 'HIGH' or finding['Severity'] == 'CRITICAL':
-                    pass
+                    self.count += 1
+                    valid = False
+        return valid
 
     def validate_bearer(self):
-        pass
+        return True
+
+    def scan(self):
+        tools = ''
+        valid = True
+        if not self.validate_zap():
+            tools += 'Zap '
+            valid = False
+            print(f'Zap scan: {valid}')
+        if not self.validate_nuclei():
+            tools += 'Nuclei '
+            valid = False
+            print(f'Nuclei scan: {valid}')
+        if not self.validate_trivy():
+            tools += 'Trivy '
+            valid = False
+            print(f'Trivy scan: {valid}')
+        if not self.validate_bearer():
+            tools += 'Bearer'
+            valid = False
+            print(f'Bearer scan: {valid}')
+
+        tools = tools.replace(' ', ', ')[:2]
+        scan_result = f'There were {self.count} high/critical severity vulnerabilities found by: {tools}'
+
+        with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
+            env_file.write(f'SCAN_VALID={valid}\n')
+
+        with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
+            env_file.write(f'SCAN_RESULT={scan_result}\n')
 
 
-val = ResultValidation()
-val.validate_trivy()
-val.set_env_variable()
+try:
+    ResultValidation().scan()
+except Exception as e:
+    print(e)
