8e90a0a1d02ac2b92e366a0d961f90d1b2d98b84
jakscha <mails-straeucher0l@icloud.com>
2024-08-16 22:35:59 +0200

    Python script for result validation

---
+++ .github/workflows/result_validation.py
@@ -0,0 +1,28 @@
+import json
+
+
+class ResultValidation:
+    def load_json_from_file(self, file_name: str) -> dict:
+        with open(f'results/{file_name}', "r") as f:
+            return json.load(f)
+
+    def validate_zap(self):
+        pass
+
+    def validate_nuclei(self):
+        pass
+
+    def validate_trivy(self):
+        data = self.load_json_from_file('trivy.json')
+        findings = data['Results']
+        print(type(findings))
+        for result in data['Results']:
+            for finding in result['Vulnerabilities']:
+                if finding['Severity'] == 'HIGH' or finding['Severity'] == 'CRITICAL':
+                    raise Exception('Critical or high vulnerability was found.')
+
+    def validate_bearer(self):
+        pass
+
+
+ResultValidation().validate_trivy()
