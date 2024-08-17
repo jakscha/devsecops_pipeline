2e8307aaf4817f774dc7e07ddfac601762482051
jakscha <mails-straeucher0l@icloud.com>
2024-08-17 18:37:37 +0200

    Changed running benchmark container in detached mode

--- VMs/runDockerImage.sh
+++ VMs/runDockerImage.sh
@@ -1,2 +1,2 @@
-docker run -t -i -p 8443:8443 --rm benchmark /bin/bash -c "git pull && ./runRemoteAccessibleBenchmark.sh"
+docker run -d -t -i -p 8443:8443 --rm benchmark /bin/bash -c "git pull && ./runRemoteAccessibleBenchmark.sh"
 
