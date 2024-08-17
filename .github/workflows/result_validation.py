7034b5ad8b7ddcb6055fa0dfe5194a1b2e86a3a3
jakscha <mails-straeucher0l@icloud.com>
2024-08-17 17:20:57 +0200

    Fix: workflow.yml

--- .github/workflows/result_validation.py
+++ .github/workflows/result_validation.py
@@ -23,13 +23,28 @@ class ResultValidation:
         return valid
 
     def validate_nuclei(self):
+        valid = True
         return True
+        data = self.load_json_from_file('nuclei.sarif')
+
+        for run in data['runs']:
+            for result in run['results']:
+                if result['level'] == 'critical' or result['level'] == 'high':
+                    self.count += 1
+                    valid = False
+        return valid
+
+    def validate_snyk(self):
+        valid = True
+        data = self.load_json_from_file('snyk.json')
+        return valid
+
 
     def validate_trivy(self):
         valid = True
         data = self.load_json_from_file('trivy.json')
         findings = data['Results']
-        print(type(findings))
+
         for result in data['Results']:
             for finding in result['Vulnerabilities']:
                 if finding['Severity'] == 'HIGH' or finding['Severity'] == 'CRITICAL':
@@ -38,27 +53,50 @@ class ResultValidation:
         return valid
 
     def validate_bearer(self):
-        return True
+        valid = True
+        data = self.load_json_from_file('bearer.json')
+        if 'high' in data:
+            self.count += len(data['high'])
+            valid = False
+        if 'critical' in data:
+            self.count += len(data['critical'])
+            valid = False
+        return valid
 
     def scan(self):
         tools = ''
         valid = True
-        if not self.validate_zap():
-            tools += 'Zap '
-            valid = False
-            print(f'Zap scan: {valid}')
-        if not self.validate_nuclei():
-            tools += 'Nuclei '
-            valid = False
-            print(f'Nuclei scan: {valid}')
-        if not self.validate_trivy():
-            tools += 'Trivy '
-            valid = False
-            print(f'Trivy scan: {valid}')
         if not self.validate_bearer():
             tools += 'Bearer'
             valid = False
-            print(f'Bearer scan: {valid}')
+            print(f'Bearer scan did not pass.')
+        else:
+            print(f'Bearer scan passed.')
+        if not self.validate_nuclei():
+            tools += 'Nuclei '
+            valid = False
+            print(f'Nuclei scan did not pass.')
+        else:
+            print(f'Nuclei scan passed.')
+        if not self.validate_zap():
+            tools += 'Zap '
+            valid = False
+            print(f'Zap scan did not pass.')
+        else:
+            print(f'Zap scan passed.')
+        if not self.validate_snyk():
+            tools += 'Snyk '
+            valid = False
+            print(f'Snyk scan did not pass.')
+        else:
+            print(f'Snyk scan passed.')
+        if not self.validate_trivy():
+            tools += 'Trivy '
+            valid = False
+            print(f'Trivy scan did not pass.')
+        else:
+            print(f'Trivy scan passed.')
+
 
         tools = tools.replace(' ', ', ')[:-2]
         scan_result = f'There were {self.count} high/critical severity vulnerabilities found by: {tools}'
@@ -69,6 +107,8 @@ class ResultValidation:
         with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
             env_file.write(f'SCAN_RESULT={scan_result}\n')
 
+
+ResultValidation().scan()
 
 try:
     ResultValidation().scan()
